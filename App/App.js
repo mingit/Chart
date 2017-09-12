f63b0af9ce62b072711c74a29541e37699032af3
Ming Li <mWX474799@china.huawei.com>
2017-09-12 15:38:37 +0300

    update

--- App/App.js
+++ App/App.js
@@ -3,39 +3,176 @@ import {
     AppRegistry,
         Text,
         View,
-        Button
+        Button,
+        StyleSheet,
+        Platform,
+        ScrollView
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { VictoryBar, VictoryPie } from "victory-native";
 
-import data1 from './data/data.json';
+import ordersJson from './data/data.json';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#e1d7cd",
+    justifyContent: "center",
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingTop: 50
+  },
+  text: {
+    fontSize: 18,
+    fontFamily: (Platform.OS === "ios") ? "Menlo" : "monospace",
+    fontWeight: "bold",
+    marginTop: 25,
+    marginBottom: 20
+  },
+  heading: {
+    fontSize: 27,
+    fontFamily: (Platform.OS === "ios") ? "Menlo" : "monospace",
+    fontWeight: "bold",
+    marginTop: 30,
+    marginBottom: 30
+  }
+});
 
 class BarScreen extends React.Component {
-    Constructorn()
-    {
-        
-    }
+  constructor(props) {
+  super(props);
+  this.state = {
+    scrollEnabled: true,
+  };
+}
+
     static navigationOptions = {title: 'Bar'};
     render() {
         const { navigate } = this.props.navigation;
+        var colors = ["red", "orange", "gold", "blue", "cyan", "green", "black", "purple", "brown", "navy", "orchid", "grey", "magenta", "wheat"];
+
+        var x = 5;
+        var label = 0;
+        var grossAmoutForBar = ordersJson.orders.map(function(item) {
+            x += 5;
+            if (label + 1 <= colors.length)
+            {
+              color  = colors[label];
+            }
+            else {
+              color = colors[0];
+            }
+
+            label += 1;
+            return {
+              x: x,
+              y: item.GrossAmount,
+              label: item.ProductId,
+              fill: color
+            };
+          });
+
+        x = 5;
+        label = 0;
+        var netAmountForBar = ordersJson.orders.map(function(item) {
+            x += 5;
+            if (label + 1 <= colors.length)
+            {
+              color  = colors[label];
+            }
+            else {
+              color = colors[0];
+            }
+
+            label += 1;
+            return {
+              x: x,
+              y: item.NetAmount,
+              label: item.ProductId,
+              fill: color
+            };
+          });
+
+          x = 5;
+          label = 0;
+          var taxAmountForBar = ordersJson.orders.map(function(item) {
+              x += 5;
+              if (label + 1 <= colors.length)
+              {
+                color  = colors[label];
+              }
+              else {
+                color = colors[0];
+              }
+
+              label += 1;
+              return {
+                x: x,
+                y: item.TaxAmount,
+                label: item.ProductId,
+                fill: color
+              };
+            });
+
+
+                      x = 5;
+                      label = 0;
+                      var quantityForBar = ordersJson.orders.map(function(item) {
+                          x += 5;
+                          if (label + 1 <= colors.length)
+                          {
+                            color  = colors[label];
+                          }
+                          else {
+                            color = colors[0];
+                          }
+
+                          label += 1;
+                          return {
+                            x: x,
+                            y: item.Quantity,
+                            label: item.ProductId,
+                            fill: color
+                          };
+                        });
+
         return (
+              <ScrollView contentContainerStyle={styles.container} scrollEnabled={this.state.scrollEnabled}>
+
                 <View>
+                <Text style={styles.text}>{"GrossAmount of each product"}</Text>
                 <VictoryBar
-                style={{ data: { fill: "tomato", opacity: 0.5 } }}
-                data={[
-                { x: 15, y: 20, label: 1, fill: "red" },
-                { x: 25, y: 30, label: 2, fill: "orange" },
-                { x: 35, y: 65, label: 3, fill: "gold" },
-                { x: 40, y: 50, label: 4, fill: "blue" },
-                { x: 45, y: 40, label: 5, fill: "cyan" },
-                { x: 50, y: 30, label: 6, fill: "green" }
-                ]}  
-                />  
+                style={{ data: { fill: "tomato", opacity: 0.5, width: 18 } }}
+                data={grossAmoutForBar}
+                />
+
+                <Text style={styles.text}>{"NetAmount of each product"}</Text>
+                <VictoryBar
+                style={{ data: { fill: "tomato", opacity: 0.5, width: 18 } }}
+                data={netAmountForBar}
+                />
+
+                <Text style={styles.text}>{"TaxAmount of each product"}</Text>
+                <VictoryBar
+                style={{ data: { fill: "tomato", opacity: 0.5, width: 18 } }}
+                data={taxAmountForBar}
+                />
+
+
+                                <Text style={styles.text}>{"Quantity of each product"}</Text>
+                                <VictoryBar
+                                style={{ data: { fill: "tomato", opacity: 0.5, width: 18 } }}
+                                data={quantityForBar}
+                                />
+
+
                 <Button
                 onPress={() => navigate('Pie')}
                 title="Show a pie graph"
                 />
                 </View>
+
+                </ScrollView>
                );
     }
 }
